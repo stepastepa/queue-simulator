ec80eb496005daa99b873f5cd90ed97108aa7055
Stepan <stepastepa@users.noreply.github.com>
2021-05-04 13:18:46 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,35 @@
+var i=1;
+
+function randomQueue() {
+    var all = participants.value;
+    i = Math.ceil(Math.random()*all);
+    return i;
+}
+
+function queueGenerate() {
+    resultNumbers.insertAdjacentHTML('beforeend', '<div class="result">101</div>');
+    var myNumber = document.querySelector(".result:last-child");
+    // highlight lucky queue number
+    var x = randomQueue();
+    myNumber.innerHTML = x;
+    if (x<=lucky.value) {
+        highlight();
+    }
+}
+function generateQueueList() {
+    resultNumbers.innerHTML = ''; // reset list
+    for (var a=1; a<=accounts.value; a++) {
+        queueGenerate();
+    }
+}
+
+var startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", generateQueueList);
+
+
+
+//////////// highlight lucky number /////////
+function highlight() {
+    var resultNumber  = document.querySelector(".result:last-child");
+    resultNumber.classList.add("highlight");
+}

1a1c3adf6de36d01aba45bae40fa4f316f0f0686
Stepan <stepastepa@users.noreply.github.com>
2021-05-09 18:24:21 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -21,6 +21,9 @@ function generateQueueList() {
     for (var a=1; a<=accounts.value; a++) {
         queueGenerate();
     }
+    if (accounts.value > 8) {
+        longList();
+    }
 }
 
 var startButton = document.querySelector(".start-button");
@@ -28,8 +31,14 @@ startButton.addEventListener("click", generateQueueList);
 
 
 
-//////////// highlight lucky number /////////
+//////////// highlight lucky number /////////////
 function highlight() {
     var resultNumber  = document.querySelector(".result:last-child");
     resultNumber.classList.add("highlight");
+}
+
+//////////// detect long list ///////////////
+function longList() {
+    var rightSection  = document.querySelector(".right");
+    rightSection.classList.add("long-list");
 }
